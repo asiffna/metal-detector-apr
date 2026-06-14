8b28a3a3094f12c61dcbfbb69feaeb9727379eca
Anfisa <asifna9@gmail.com>
2026-06-14 16:54:05 +0300

    смена ракурса

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { IETR_DATA } from "./data.js?v=21";
+import { IETR_DATA } from "./data.js?v=23";
 
-const MODEL_URL = "assets/metal_detector_named.glb?v=21";
+const MODEL_URL = "assets/metal_detector_named.glb?v=23";
 const MARKER_TARGETS = {
   case: ["case", "case-1"],
   grille: ["eBom-prt36", "eBom-prt36-1"],
